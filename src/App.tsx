c02769f44c3fadb59c37d252f0d0768883a259b7
🎩 Ȥ𐋎𐊪ᗫ𐊤ᖇ ញαⵅᏇᗴᘂᘂ 🔱 <zandermaxwell@hey.com>
2024-01-04 16:20:56 -0500

    Adding linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.scss';
+import '@fontsource/inter';
 import Header from './Header';
 import Main from './Main';
 
-function App() {
+const App = () => {
 	return (
 		<>
 			<Header />
 			<Main />
 		</>
 	);
-}
+};
 
 export default App;
