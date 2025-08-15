e06c57d0f94418d1a4be882aa0fb71ae95ea904c
🎩 Ȥ𐋎𐊪ᗫ𐊤ᖇ ញαⵅᏇᗴᘂᘂ 🔱 <zandermaxwell@hey.com>
2025-08-15 19:50:20 +0200

    Fixing build errors (linting)

--- src/helpers/text-parser.helper.tsx
+++ src/helpers/text-parser.helper.tsx
@@ -1,16 +1,17 @@
 export const parseText = (text: string): JSX.Element => {
 	// Wrap text that has {link address=url} and {/link} in a link element
 	// Using address as the href and text as the link text
-	const linkRegex = /\{link address=(.+?)\}(.+?)\{\/link\}/g;
+	// eslint-disable-next-line regexp/no-super-linear-backtracking
+	const linkRegex = /\{link address=(.+?)\}(.+?)\{\/link\}/gu;
 	const parsedText = text.split(linkRegex).map((part, index) => {
 		if (index % 3 === 0) {
 			// This is the text outside of the link
 			return part;
-		} else if (index % 3 === 1) {
+		} else {
 			// This is the URL, and the next part is the link text
 			const url = part.trim();
 			return (
-				<a href={url} rel="noreferrer" target="_blank">
+				<a href={url} key={url} rel="noreferrer" target="_blank">
 					{text.split(linkRegex)[index + 1].trim()}
 				</a>
 			);
